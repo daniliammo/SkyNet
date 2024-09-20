b8f25e808116fcf597e569c9bad1f6ff64f98c54
daniliammo <67186086+daniliammo@users.noreply.github.com>
2024-09-21 01:01:48 +0500

    Создание тестового окна

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using ImGuiNET;
-using SFML.Audio;
+﻿// using ImGuiSharp;
+using SFML.Graphics;
+using SFML.Window;
 
 
 namespace SkyNet;
@@ -9,9 +10,39 @@ internal abstract class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Привет, Мир!");
-        Console.WriteLine(ImGui.GetVersion());
-        var x = false;
-        ImGui.ShowDemoWindow(ref x);
+        // Console.WriteLine(ImGui.GetVersion());
+
+        var mode = new VideoMode(1920, 1080);
+        var window = new RenderWindow(mode, "SkyNet", Styles.Close);
+        
+        window.SetVerticalSyncEnabled(true);
+
+        window.Closed += (_, _) => window.Close();
+
+        // ImGui.CreateContext(new ImFontAtlas());
+        // ImGui.NewFrame();
+        // ImGui.Render();
+
+        var shape = new CircleShape(360);
+        shape.FillColor = Color.Green;
+        
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+            
+            window.Clear(Color.Transparent);
+            window.Draw(shape);
+            window.Display();
+            
+            // ImGui.NewFrame();
+            // var unused2 = false;
+            // ImGui.ShowDemoWindow(ref unused2);
+            // ImGui.Render();
+        }
+
+        // ImGui.DestroyContext(null);
+        
+        
         new SkyNet();
 
         // var buffer = new SoundBuffer("/home/daniliammo/HookCanceled.wav");
