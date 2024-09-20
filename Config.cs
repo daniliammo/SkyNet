a9174fcfc4dc07ef4d2a1eef2524d0df18970ba7
daniliammo <67186086+daniliammo@users.noreply.github.com>
2024-09-20 21:32:49 +0500

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+
+namespace SkyNet;
+
+public static class Config
+{
+    #region Colors
+    public static readonly Color AllyGadgetsColor = Color.Aqua;
+    public static readonly Color EnemyGadgetsColor = Color.Chocolate;
+    public static readonly Color GrenadeColor = Color.Magenta;
+    public static readonly Color EnemyColor = Color.Firebrick;
+    public static readonly Color AllyColor = Color.Chartreuse;
+    public static readonly Color DeadBodyColor = Color.Black;
+    #endregion
+}
