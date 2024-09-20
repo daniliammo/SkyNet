05a875d54bbc2e4b0a2fbcc0a0067a123099af6b
daniliammo <67186086+daniliammo@users.noreply.github.com>
2024-09-21 01:15:26 +0500

    Добавлены все типы техники

--- ObjectType.cs
+++ ObjectType.cs
@@ -18,15 +18,63 @@ public enum ObjectType
     GasTigerUnarmed,
     GasTigerCrows,
     GasTigerStandardM2Browning,
+    
+    GasTigerUnarmedDestroyed,
+    GasTigerCrowsDestroyed,
+    GasTigerStandardM2BrowningDestroyed,
     #endregion
 
     #region Humvee
-    HumveeUnarmed,
-    HumveeCrows,
-    HumveeStandardM2Browning,
+    HumveeUnarmedDestroyed,
+    HumveeCrowsDestroyed,
+    HumveeStandardM2BrowningDestroyed,
+    #endregion
+
+    #region Tanks
+    M1Abrams,
+    T90A,
+    M1AbramsDestroyed,
+    T90ADestroyed,
+    #endregion
+
+    #region Sea Vehicles
+    Pwc,
+    PwcDestroyed,
+    Rhib,
+    RhibDestroyed,
+    Rcb90,
+    Rcb90Destroyed,
+    #endregion
+
+    #region Armored Personnel Carrier
+    Lav25A1,
+    Lav25A1Destroyed,
+    Btr82,
+    Btr82Destroyed,
+    #endregion
+    
+    #region Hehicopters
+    BlackHawk,
+    BlackHawkDestroyed,
+    
+    Ka90,
+    Ka90Destroyed,
+    
+    LittleBird,
+    LittleBirdDestroyed,
+    LittleBirdMilitaryArmed,
+    LittleBirdMilitaryArmedDestroyed,
+    
+    Hermit,
+    HermitDestroyed,
+    HermitMilitary,
+    HermitMilitaryDestroyed,
+    HermitHermitMilitaryArmed,
+    HermitHermitMilitaryArmedDestroyed,
     #endregion
     
     Quad,
+    QuadDestroyed,
     
     #endregion
     
