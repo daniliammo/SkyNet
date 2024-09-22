a75d06505f09ad6d287b60d9bcd517b3df4e3fcd
daniliammo <67186086+daniliammo@users.noreply.github.com>
2024-09-22 22:28:52 +0500

    System.Drawing.Color теперь не используется. Все  цвета будут иметь класс Cairo.Color

--- Config.cs
+++ Config.cs
@@ -1,17 +1,21 @@
-using System.Drawing;
+using Cairo;
 
 namespace SkyNet;
 
 public static class Config
 {
     #region Colors
-    public static readonly Color AllyGadgetsColor = Color.Aqua;
-    public static readonly Color EnemyGadgetsColor = Color.Chocolate;
-    public static readonly Color GrenadeColor = Color.Magenta;
-    public static readonly Color EnemyColor = Color.Firebrick;
-    public static readonly Color AllyColor = Color.Chartreuse;
-    public static readonly Color DeadBodyColor = Color.Black;
+    public static readonly Color AllyGadgetsColor = new (0, 255, 255); // rgb(0, 255, 255)
+    public static readonly Color EnemyGadgetsColor = new (210, 105, 30); // rgb(210, 105, 30)
+    public static readonly Color GrenadeColor = new (255, 0, 255); // rgb(255, 0, 255)
+    public static readonly Color EnemyColor = new (178, 34, 34); // rgb(178, 34, 34)
+    public static readonly Color AllyColor = new (127, 255, 0); // rgb(127, 255, 0)
+    public static readonly Color DeadBodyColor = new(0, 0, 0); // rgb(0, 0, 0)
+    #endregion
+    
+    public static readonly Color CrosshairColor = new(0, 255, 127); // rgb(0, 255, 127)
+    public const bool DrawCrosshair = true;
 
     public static readonly byte Fps = 165;
-    #endregion
+    
 }
--- Program.cs
+++ Program.cs
@@ -56,44 +56,75 @@ public static class WindowController
         _context.SetSourceRGBA(0.0, 0.0, 0.0, 0.0);
         _context.Paint();
         
-        DrawCircles();
+        DrawScreenBounds();
+
+        SkyNet.VisionObjects =
+        [
+            new VisionObject(ObjectType.Ally, new Transform(new Vector2(500, 356), new Vector2(100, 52)))
+            {
+                Transform = new Transform(new Vector2(760, 456), new Vector2(50, 180))
+            }
+        ];
+
+        if(Config.DrawCrosshair)
+            DrawCrosshair();
         
-        // foreach (var visionObject in SkyNet.VisionObjects!)
-        //     DrawObject(visionObject);
+        foreach (var visionObject in SkyNet.VisionObjects!)
+            DrawObject(visionObject);
         
         // Завершить рисование
         _context.Dispose();
     }
 
-    private static void DrawCircles()
+    private static void DrawCrosshair()
+    {
+        _context!.SetSourceColor(Config.CrosshairColor);
+        
+        DrawCircle(new Vector2(_widget!.Window.Width / 2, _widget!.Window.Height / 2), 7);
+    }
+    
+    private static void DrawScreenBounds()
     {
         // Цвет круга (зеленый)
         _context!.SetSourceRGB(0.0, 1.0, 0.0);
         
-        _context.NewPath();
-        _context.Arc(0, 0, 15, 0, 2 * Math.PI);
+        DrawCircle(new Vector2(0, 0), 15);
+        DrawCircle(new Vector2(_widget!.Window.Width, 0), 15);
+        DrawCircle(new Vector2(_widget.Window.Width, _widget.Window.Height), 15);
+        DrawCircle(new Vector2(0, _widget.Window.Height), 15);
+    }
+
+    private static void DrawCircle(Vector2 position, double radius)
+    {
+        _context!.NewPath();
+        _context.Arc(position.X, position.Y, radius, 0, 2 * Math.PI);
         _context.FillPreserve();
-        
-        _context.NewPath();
-        _context.Arc(_widget!.Window.Width, _widget.Window.Height, 15, 0, 2 * Math.PI);
-        _context.FillPreserve();
-        
-        _context.NewPath();
-        _context.Arc(_widget.Window.Width, 0, 15, 0, 2 * Math.PI);
-        _context.FillPreserve();
-        
-        _context.NewPath();
-        _context.Arc(0, _widget.Window.Height, 15, 0, 2 * Math.PI);
-        _context.FillPreserve();
+    }
+    
+    private static void DrawText(string text, double x, double y)
+    {
+        // Установить шрифт и размер
+        _context!.SetFontSize(32); // Размер шрифта 40
+        _context.MoveTo(x, y); // Позиция для текста
+    
+        // Получить ширину текста для центрирования
+        var textExtents = _context.TextExtents(text);
+        // Задать точку для отрисовки текста 1/2 ширины текста влево от заданной точки
+        _context.MoveTo(x - textExtents.Width / 2, y + textExtents.Height / 2); // Вертикально центрируем текст
+    
+        // Цвет текста (черный)
+        _context.SetSourceRGB(0.0, 0.0, 0.0); // Черный цвет
+        _context.ShowText(text); // Отрисовать текст
     }
     
     private static void DrawObject(VisionObject visionObject)
     {
-        _context!.SetSourceRGB(visionObject.StrokeColor.R, visionObject.StrokeColor.G, visionObject.StrokeColor.B);
+        _context!.SetSourceColor(visionObject.StrokeColor);
         DrawBox(visionObject.Transform, visionObject.StrokeColor);
+        DrawText(visionObject.ObjectType.ToString(), visionObject.Transform.Position.X, visionObject.Transform.Position.Y * 0.925f);
     }
     
-    private static void DrawBox(Transform transform, System.Drawing.Color color)
+    private static void DrawBox(Transform transform, Color color)
     {
         _context!.SetSourceRGB(color.R, color.G, color.B);
         _context.Rectangle(transform.Position.X, transform.Position.Y, transform.Scale.X, transform.Scale.Y); // Рисуем квадрат
